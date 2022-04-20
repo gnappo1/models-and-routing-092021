6edf1eadf58d1932c26ac284b263764a9ea89328
Matteo <gnappo.1@live.it>
2022-04-20 16:21:25 -0700

    Allow cors and test removing validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :post #will automatically validate post_id existance
   validates :rating, presence: true, numericality: {less_than_or_equal_to: 5, greater_than_or_equal_to: 1}
   validates :content, presence: true, length: {in: 2..1000}#, inclusion: {in: %w(shoot fudge accidempoli), message: "You used inappropriate word(s). Please make sure you avoid any of the following: #{@@bad_words.join(", ")}"}
-  validate :respectable_words_check
+  # validate :respectable_words_check
 
   def respectable_words_check
     bad_words = %w(shoot fudge accidempoli)
