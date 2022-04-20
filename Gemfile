792156ec46221a98e90504e13d7754168e28acfb
Matteo <gnappo.1@live.it>
2022-04-20 02:06:16 -0700

    Prepare for deployment to Heroku

--- Gemfile
+++ Gemfile
@@ -5,8 +5,6 @@ ruby '2.7.4'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.5'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -21,11 +19,13 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
-
+gem 'pg'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 gem 'rack-cors'
 
 group :development, :test do
+  # Use sqlite3 as the database for Active Record
+  gem 'sqlite3', '~> 1.4'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   # gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry'
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
   # @@bad_words = %w(shoot fudge accidempoli)
   belongs_to :post #will automatically validate post_id existance
-  validates :rating, presence: true, numericality: {less_than_or_equal_to: 5, greater_than_or_equal_to: 1}
-  validates :content, presence: true, length: {in: 2..1000}#, inclusion: {in: %w(shoot fudge accidempoli), message: "You used inappropriate word(s). Please make sure you avoid any of the following: #{@@bad_words.join(", ")}"}
+  # validates :rating, presence: true, numericality: {less_than_or_equal_to: 5, greater_than_or_equal_to: 1}
+  # validates :content, presence: true, length: {in: 2..1000}#, inclusion: {in: %w(shoot fudge accidempoli), message: "You used inappropriate word(s). Please make sure you avoid any of the following: #{@@bad_words.join(", ")}"}
   validate :respectable_words_check
 
   def respectable_words_check
