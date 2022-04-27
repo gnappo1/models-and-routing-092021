7662a15ed97bbc21d343a920e48aa993389f53b5
Matteo <gnappo.1@live.it>
2022-04-26 17:09:33 -0700

    Add helpers for authentication and authorization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,9 @@
 class SessionsController < ApplicationController
+    skip_before_action :authorized!, only: [:create]
 
     def create
         user = User.find_by_email(params[:email])
+        binding.pry
         if user&.authenticate(params[:password])
             session[:user_id] = user.id #line responsible for signing someone in
             render json: UserSerializer.new(user), status: :accepted
